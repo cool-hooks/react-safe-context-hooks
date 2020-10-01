60fa7a60c7cd53ef3e1a36ee60fe7e3a821aaa4d
Jakub Biesiada <biesiadajakub@icloud.com>
2020-10-01 19:27:14 +0200

    :wrench: update tsconfig

--- src/useSafeContext.ts
+++ src/useSafeContext.ts
@@ -4,6 +4,7 @@ export const useSafeContext = <T>(unsafeContext: Context<T>) => {
   const context = useContext<T>(unsafeContext);
 
   if (!context) {
+    // TODO: fix context name
     throw new Error(`Missing context ${unsafeContext.displayName}`);
   }
 
